2031cb74e0720f6a09640c6b327468f13b1934df
untitled-dream <untitled_dream@inbox.ru>
2022-07-27 13:17:51 +0300

    Go to Main

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,18 @@
 import React from "react";
 
-import { PlayContext, playInfo } from "../context/PlayContext";
 import Header from "./Header";
 import Main from "./Main";
-import PhotoGallery from "./PhotoGallery";
-import MediaReview from "./MediaReview";
-import Review from "./Review";
-import SocialShare from "./SocialShare";
 import Footer from "./Footer";
+
 import Scrolling from "./Scrolling";
-import Project from "./Project";
 
 function App() {
   return (
     <>
       <Header />
+      <Main/>
+      <Footer />
       <Scrolling />
-      <main className="content">
-        <PlayContext.Provider value={playInfo['s-uchilisha']} >
-          <Project />
-          {/*<section className="gallery">
-            <h2 className="gallery__title">Заголовок&nbsp;блока с&nbsp;фотографиями</h2>
-            <ul className="gallery__grid list">
-              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-1.jpg')%>" alt="Сцена из спектакля" /></li>
-              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-2.jpg')%>" alt="актриса на сцене" /></li>
-              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-3.jpg')%>" alt="Актер на репетиции" /></li>
-              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-4.jpg')%>" alt="Актеры по ролям читают пьесу" /></li>
-              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-5.jpg')%>" alt="Зрители в зале" /></li>
-              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-6.jpg')%>" alt="Молодая актриса" /></li>
-              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-7.jpg')%>" alt="Артисты читают пьесу" /></li>
-              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-8.jpg')%>" alt="Актриса на репетиции" /></li>
-            </ul>
-          </section>*/}
-          <MediaReview />
-          <SocialShare />
-        </PlayContext.Provider>
-        <Footer/>
-      </main>
       
       <div className="popup" id="photo-gallery-view">
         <div className="popup__container">
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,35 @@
 import React from 'react'
 
+import { PlayContext, playInfo } from "../context/PlayContext";
+
+import Review from "./Review";
+import Project from "./Project";
+import MediaReview from "./MediaReview";
+import SocialShare from "./SocialShare";
+import PhotoGallery from "./PhotoGallery";
+
 function Main() {
   return (
-    <div>Main</div>
+    <main className="content">
+      <PlayContext.Provider value={playInfo['s-uchilisha']} >
+        <Project />
+        {/*<section className="gallery">
+            <h2 className="gallery__title">Заголовок&nbsp;блока с&nbsp;фотографиями</h2>
+            <ul className="gallery__grid list">
+              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-1.jpg')%>" alt="Сцена из спектакля" /></li>
+              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-2.jpg')%>" alt="актриса на сцене" /></li>
+              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-3.jpg')%>" alt="Актер на репетиции" /></li>
+              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-4.jpg')%>" alt="Актеры по ролям читают пьесу" /></li>
+              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-5.jpg')%>" alt="Зрители в зале" /></li>
+              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-6.jpg')%>" alt="Молодая актриса" /></li>
+              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-7.jpg')%>" alt="Артисты читают пьесу" /></li>
+              <li className="gallery__item"><img className="gallery__image" src="<%=require('./images/show-photo-8.jpg')%>" alt="Актриса на репетиции" /></li>
+            </ul>
+          </section>*/}
+        <MediaReview />
+        <SocialShare />
+      </PlayContext.Provider>
+    </main>
   )
 }
 
