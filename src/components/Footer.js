7a1c8df1b52982a146b6e803d3769da20ee02dc8
untitled-dream <untitled_dream@inbox.ru>
2022-07-27 11:58:54 +0300

    footer logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,30 +3,30 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="footer" id="contacts">
-      <a className="footer__logo-link" href="#" target="_blank"><img className="footer__logo" src="./images/logo-text.svg" alt="Логотип фестиваля Любимовка" /></a>
+      <a className="footer__logo-link" href="#" target="_blank"><img className="footer__logo" src="./images/footer/logo-text.svg" alt="Логотип фестиваля Любимовка" /></a>
       <nav className="footer__partners-block">
         <h3 className="footer__partners-heading">Генеральные партнеры</h3>
         <ul className="footer__partners-list list">
           <li className="footer__partners-list-item"> 
             <a className="footer__partners-link" href="https://www.prokhorovfund.ru" target="_blank" rel="noopener noreferrer">
-              <img className="footer__partners-logo" src="./images/footer/partner/partner-prokhorov.svg" alt="Логотип Фонда Михаила Прохорова" />
+              <img className="footer__partners-logo" src="./images/footer/partner-logo/partner-prokhorov.svg" alt="Логотип Фонда Михаила Прохорова" />
             </a>
             <p className="footer__partners-subheading">Генеральный партнёр</p>
           </li>
           <li className="footer__partners-list-item">
             <a className="footer__partners-link" href="https://mosbrew.ru" target="_blank" rel="noopener noreferrer">
-              <img className="footer__partners-logo" src="./images/footer/partner/partner-mosbrew.svg" alt="Логотип Московской пивоваренной компании" />
+              <img className="footer__partners-logo" src="./images/footer/partner-logo/partner-mosbrew.svg" alt="Логотип Московской пивоваренной компании" />
             </a>
           </li>
           <li className="footer__partners-list-item">
             <a className="footer__partners-link" href="https://sbtg.ru" target="_blank" rel="noopener noreferrer">
-              <img className="footer__partners-logo" src="./images/footer/partner/partner-sbtg.svg" alt="Логотип видеокомпании SBTG" />
+              <img className="footer__partners-logo" src="./images/footer/partner-logo/partner-sbtg.svg" alt="Логотип видеокомпании SBTG" />
             </a>
             <p className="footer__partners-subheading">Онлайн-трансляции</p>
           </li>
           <li className="footer__partners-list-item">
             <a className="footer__partners-link" href="https://vk.com/public211134323" target="_blank" rel="noopener noreferrer">
-              <img className="footer__partners-logo" src="./images/footer/partner/partner-a-pogovorit.svg" alt="Логотип подкаста А поговорить" />
+              <img className="footer__partners-logo" src="./images/footer/partner-logo/partner-a-pogovorit.svg" alt="Логотип подкаста А поговорить" />
             </a>
           </li>
         </ul>
@@ -67,7 +67,7 @@ function Footer() {
       <hr className="footer__separator" />
       <p className="footer__copyright">©&nbsp;Любимовка,&nbsp;2021 <a href="#" className="footer__link link link_underline footer__privacy-policy" target="_blank">Политика&nbsp;конфиденциальности</a></p>
       <div className="footer__credits-design">
-        <img className="footer__credits-design-logo" src="./images/footer/icon-developer-studio-logo" alt="Логотип дизайн-студии" />
+        <img className="footer__credits-design-logo" src="./images/footer/icon-developer-studio-logo.svg" alt="Логотип дизайн-студии" />
         <p className="footer__credits-design-text">дизайн сайта — <a className="footer__link link link_underline" href="https://ru.shishki.co" target="_blank" rel="noopener noreferrer">shishki.collective</a></p>
       </div>
       <p className="footer__credits-web">вёрстка и разработка — студенты <a className="footer__link link link_underline" href="https://practicum.yandex.ru" target="_blank" rel="noopener noreferrer">Яндекс.Практикума</a></p>
