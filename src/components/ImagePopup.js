4e23a7fb3d58434f26bec6299c66dc99afc9e387
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-08-05 12:04:13 +0300

    refactor: добавить ReactModal в попап галереи

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,52 +1,77 @@
-import { useContext, useRef, useEffect } from 'react'
+import { useContext } from 'react';
 
-import { PlayContext } from "../context/PlayContext";
+import { PlayContext } from '../context/PlayContext';
 
 import { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import ReactModal from 'react-modal';
 
 function ImagePopup({ isOpen, photoIndex, onClose }) {
-
-  const swiperRef = useRef(null)
-  useEffect(() => {
-    swiperRef.current.swiper.slideTo(photoIndex, false);
-  }, [photoIndex])
-  
   const { id, photoGallery } = useContext(PlayContext);
 
   return (
-    <div className={isOpen ? 'popup popup_opened' : 'popup'} id="photo-gallery-view">
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      shouldCloseOnOverlayClick={true}
+      shouldCloseOnEsc={true}
+      style={{
+        overlay: { 
+          backgroundColor: 'rgba(0, 0, 0, 0.9', 
+          zIndex: 2 },
+        content: {
+          display: 'flex',
+          alignItems: 'center',
+          background: 'none',
+          border: 'none',
+        },
+      }}
+    >
       <Swiper
-        ref={swiperRef}
-        
         modules={[Navigation]}
+        initialSlide={photoIndex}
         navigation={{
           prevEl: '.slider__button-prev',
           nextEl: '.slider__button-next',
-        }
-        }
-
+        }}
         grabCursor={true}
         rewind={true}
       >
-        {
-          photoGallery.map((item, i) => (
-            <SwiperSlide key={i} className='popup__image-container'>
-              {
-                <img className="popup__gallery-image" src={require(`../images/plays/${id}/gallery/${item}`)} alt="Описание" />
-              }
-            </SwiperSlide>
-          ))
-        }
+        {photoGallery.map((item, i) => (
+          <SwiperSlide key={i} className='popup__image-container'>
+            {
+              <img
+                className='popup__gallery-image'
+                src={require(`../images/plays/${id}/gallery/${item}`)}
+                alt='Описание'
+              />
+            }
+          </SwiperSlide>
+        ))}
       </Swiper>
-      <button className="popup__button-close button" onClick={onClose} type="button" title="Закрыть окно" />
-      <button className="popup__slider-button slider__button-prev button" type="button" name='prev' title="Предыдущая фотография" />
-      <button className="popup__slider-button slider__button-next button" type="button" name='next' title="Следующая фотография" />
-    </div>
-  )
+      <button
+        className='popup__button-close button'
+        onClick={onClose}
+        type='button'
+        title='Закрыть окно'
+      />
+      <button
+        className='popup__slider-button slider__button-prev button'
+        type='button'
+        name='prev'
+        title='Предыдущая фотография'
+      />
+      <button
+        className='popup__slider-button slider__button-next button'
+        type='button'
+        name='next'
+        title='Следующая фотография'
+      />
+    </ReactModal>
+  );
 }
 
-export default ImagePopup
+export default ImagePopup;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,15 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import { PlayContext, playInfo } from "../context/PlayContext";
+import { PlayContext, playInfo } from '../context/PlayContext';
 
-import Review from "./Review";
-import Project from "./Project";
-import SocialShare from "./SocialShare";
-import PhotoGallery from "./PhotoGallery";
+import Review from './Review';
+import Project from './Project';
+import SocialShare from './SocialShare';
+import PhotoGallery from './PhotoGallery';
 
 import ImagePopup from './ImagePopup';
 
 function Main() {
-
   const [isViewImagePopupOpen, setViewImagePopupOpen] = useState(false);
   const [selectedPhotoIndex, setSelectedPhoto] = useState();
 
@@ -20,26 +19,25 @@ function Main() {
   }
 
   function closeAllPopups() {
-    setViewImagePopupOpen(false)
+    setViewImagePopupOpen(false);
   }
 
   return (
-    <main className="content">
-      <PlayContext.Provider value={playInfo['s-uchilisha']} >
+    <main className='content'>
+      <PlayContext.Provider value={playInfo['s-uchilisha']}>
         <Project />
         <PhotoGallery onPhotoClick={handlePhotoClick} />
         <Review />
         <SocialShare />
 
-        {isViewImagePopupOpen &&
-          <ImagePopup
-            isOpen={isViewImagePopupOpen}
-            photoIndex={selectedPhotoIndex}
-            onClose={closeAllPopups}
-          />}
+        <ImagePopup
+          isOpen={isViewImagePopupOpen}
+          photoIndex={selectedPhotoIndex}
+          onClose={closeAllPopups}
+        />
       </PlayContext.Provider>
     </main>
-  )
+  );
 }
 
-export default Main
+export default Main;
